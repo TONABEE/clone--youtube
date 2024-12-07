4b2d70664f3dfbc10851dae87089d4f063d3829d
TONABEE <nakataayuki@gmail.com>
2024-12-07 17:03:12 +0900

    Deploy build files to gh-pages

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/clone--youtube/', // リポジトリ名を指定
+});
+
+
